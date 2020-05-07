6dff62d921d12d872f51ef02b4b0294b3195f55c
Martyna Kuryłowicz <martynakurylowicz@gmail.com>
2020-05-07 15:31:40 +0200

    new classes - BEM

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Cześć! To strona nauki WTF Martyny");
+const firstName = "Martyn";
+const age = 32;
+
+console.log(
+    `Cześć! To strona nauki WTF. Notatki przygotowała ${firstName}.`
+    );
