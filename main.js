001295a080b078054b829df7e0da1dcec68295c4
Martyna Kuryłowicz <martynakurylowicz@gmail.com>
2020-05-14 18:42:41 +0200

    homework week 5

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ console.log(
 const task = document.querySelector('.task-js');
 
 task.innerHTML = `Cześć! To strona nauki WTF. Notatki przygotowała ${firstName}.`
+
+function hello(firstName, age) {
+    console.log(
+        `Cześć! To strona nauki WTF. Notatki przygotowała ${firstName} i ma ${age} lata.`
+        );
+}
+hello(firstName, age);
