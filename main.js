b931a007f91c304eb4b138af1823c5615517941e
Martyna Kuryłowicz <martynakurylowicz@gmail.com>
2020-05-23 14:15:47 +0200

    hamburger menu

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 const firstName = "Martyn";
 const age = 32;
 
-console.log(
-    `Cześć! To strona nauki WTF. Notatki przygotowała ${firstName}.`
-    );
+console.log(`Cześć! To strona nauki WTF. Notatki przygotowała ${firstName}.`);
 
-const task = document.querySelector('.task-js');
+const task = document.querySelector(".task-js");
 
-task.innerHTML = `Cześć! To strona nauki WTF. Notatki przygotowała ${firstName}.`
+task.innerHTML = `Cześć! To strona nauki WTF. Notatki przygotowała ${firstName}.`;
 
 function hello(firstName, age) {
-    console.log(
-        `Cześć! To strona nauki WTF. Notatki przygotowała ${firstName} i ma ${age} lata.`
-        );
+  console.log(
+    `Cześć! To strona nauki WTF. Notatki przygotowała ${firstName} i ma ${age} lata.`
+  );
 }
 hello(firstName, age);
+
+const hamburger = document.querySelector(".navigation__hamburger-menu--js");
+
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector(".navigation--js");
+  nav.classList.toggle("navigation--open");
+});
